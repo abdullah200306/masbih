a2dda94a57a0e76d33f30b4c20decc41e6f9039a
abdullah200306 <146236693+abdullah200306@users.noreply.github.com>
2025-10-24 20:41:06 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 // import "./style.css"; // فعل إذا نقلنا style.css لـ src
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
