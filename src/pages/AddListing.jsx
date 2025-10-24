0805d16985a93afdae5d9c2647df41a7aa67b49a
abdullah200306 <146236693+abdullah200306@users.noreply.github.com>
2025-10-24 22:03:46 +0300

    Create AddListing.jsx

---
+++ src/pages/AddListing.jsx
@@ -0,0 +1,8 @@
+export default function AddListing() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>إضافة إعلان جديد</h2>
+      <p>الميزة قيد التطوير 👷‍♂️✨</p>
+    </div>
+  );
+}
