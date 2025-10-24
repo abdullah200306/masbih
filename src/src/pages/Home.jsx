bdbb942e21be1e3e744a1ff09d8e5fe7e464b32a
abdullah200306 <146236693+abdullah200306@users.noreply.github.com>
2025-10-24 20:06:03 +0300

    Delete src/src/pages/Home.jsx

--- src/src/pages/Home.jsx
+++
@@ -1,25 +0,0 @@
-export default function Home() {
-  return (
-    <div className="page">
-      <header className="topbar">
-        <div className="brand">
-          <span className="logo-dot">🧿</span>
-          <span className="brand-text">مِسباحي</span>
-        </div>
-        <input className="search-input" placeholder="🔍 ابحث عن مسباح..." />
-      </header>
-
-      <div className="grid">
-        {[1,2,3,4,5,6].map((i) => (
-          <div className="card" key={i}>
-            <div className="img-wrap">
-              <img src={`https://picsum.photos/seed/misbah${i}/400/300`} alt="misbaha" />
-            </div>
-            <h3>مسباح مثال #{i}</h3>
-            <p className="price">10 د.ك</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
